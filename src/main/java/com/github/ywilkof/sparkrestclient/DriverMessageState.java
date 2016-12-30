07f42072ea15e6714273ca9729880fc46ab4ec6b
kui.liu <417430414@qq.com>
2016-12-30 10:15:43 +0800

    D

--- src/main/java/com/github/ywilkof/sparkrestclient/DriverMessageState.java
+++ src/main/java/com/github/ywilkof/sparkrestclient/DriverMessageState.java
@@ -4,5 +4,5 @@ package com.github.ywilkof.sparkrestclient;
  * Created by yonatan on 09.10.15.
  */
 public enum DriverMessageState {
-	TASK_FINISHED, TASK_FAILED;
+	TASK_FINISHED, TASK_FAILED,TASK_RUNNING,TASK_SUBMITTED,TASK_RELAUNCHING,TASK_UNKNOWN,TASK_KILLED,TASK_ERROR,TASK_NOT_FOUND;
 }
